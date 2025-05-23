7acfb14244f962af38672519dd44094c3535e5b3
Lucas-erro <cristiancaue36@gmail.com>
2025-05-23 00:59:47 +0000

    fiz o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,52 +1,6 @@
 #include <stdio.h>
 
-int main(){
-    printf("Desafio Super Trunfo Novato!/n");
-    printf("novo commit/n");
-    // Variáveis da Carta 1
-    char estado1;
-    char codigo1[5];
-    char nome1[50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontos_turisticos1;
-
-    // Variáveis da Carta 2
-    char estado2;
-    char codigo2[5];
-    char nome2[50];
-    int populacao2;
-    float area2;
-    float pib2;
-    int pontos_turisticos2;
-
-    // Entrada de dados da Carta 1
-    printf("Informe os dados da Carta 1:\n");
-
-    printf("Estado (A-H): ");
-    scanf(" %c", &estado1);
-    getchar(); // Limpa o Enter do buffer
-
-    printf("Codigo da Carta (ex: A01): ");
-    fgets(codigo1, sizeof(codigo1), stdin);
-    codigo1[strcspn(codigo1, "\n")] = '\0'; // Remove '\n'
-
-    printf("Nome da Cidade: ");
-    fgets(nome1, sizeof(nome1), stdin);
-    nome1[strcspn(nome1, "\n")] = '\0';
-
-    printf("Populacao: ");
-    scanf("%d", &populacao1);
-
-    printf("Area (km²): ");
-    scanf("%f", &area1);
-
-    printf("PIB (em bilhoes): ");
-    scanf("%f", &pib1);
-
-    printf("Numero de Pontos Turisticos: ");
-    scanf("%d", &pontos_turisticos1);
-
-    getchar(); // Limpa o Enter do último scanf
-}
+int main() {
+    printf("Desafio Super Trunfo Novato!\n");
+    printf("novo commit\n\n");
+}    
