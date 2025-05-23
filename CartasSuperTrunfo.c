35cea5a4f12dd3dc4ae488a83a294a0a9014d110
Lucas-erro <cristiancaue36@gmail.com>
2025-05-23 00:15:04 +0000

    variáveis carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,4 +3,13 @@
 int main(){
     printf("Desafio Super Trunfo Novato!/n");
     printf("novo commit/n");
+    // Variáveis da Carta 1
+    char estado1;
+    char codigo1[5];
+    char nome1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos_turisticos1;
+
 }
